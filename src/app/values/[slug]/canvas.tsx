99958115852db44f84c87c200e4981cab227bbc9
logix <logixism@pm.me>
2025-02-20 04:04:32 +0200

    controls

--- src/app/values/[slug]/canvas.tsx
+++ src/app/values/[slug]/canvas.tsx
@@ -4,6 +4,7 @@ import * as THREE from "three";
 import { useFrame, useLoader } from "@react-three/fiber";
 import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader.js";
 import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 import { useEffect, useState } from "react";
 
 export default function ThreeCanvas() {
@@ -20,19 +21,21 @@ export default function ThreeCanvas() {
 
   return (
     <div className="h-55">
-        <Canvas
-          camera={{
-            fov: 45,
-            position: [0, -45, 185],
-            up: [0, 1, 0],
-          }}
-        >
-          <pointLight position={[0, 50, -50]} intensity={10} decay={0.5} />
-          <pointLight position={[0, 50, 150]} intensity={10} decay={0.5} />
-          <mesh rotation={[0.5, yRotation, 0]}>
-            <primitive object={model} />
-          </mesh>
-        </Canvas>
+      <Canvas
+        camera={{
+          fov: 45,
+          position: [0, -45, 185],
+          up: [0, 1, 0],
+        }}
+      >
+        <OrbitControls />
+        <hemisphereLight intensity={3} />
+        <pointLight position={[0, 40, 185]} intensity={5} decay={0.2} />
+        <pointLight position={[0, -25, 185]} intensity={5} decay={2} />
+        <mesh>
+          <primitive object={model} />
+        </mesh>
+      </Canvas>
     </div>
   );
 }
