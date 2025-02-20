21fcb4e61835cde771b6d712be0907e686099200
rostudio <103057044+rostudiosite@users.noreply.github.com>
2025-02-19 17:40:37 -0800

    3d model adjustments

--- src/app/values/[slug]/canvas.tsx
+++ src/app/values/[slug]/canvas.tsx
@@ -9,18 +9,6 @@ import { useEffect, useState } from "react";
 export default function ThreeCanvas() {
   const model = useLoader(FBXLoader, "/proto.fbx");
 
-  model.traverseVisible((object) => {
-    if (object instanceof THREE.Mesh) {
-      object.material = new THREE.MeshPhysicalMaterial({
-        color: 0xcccccc,
-        clearcoat: 0.2,
-        roughness: 0.45,
-        reflectivity: 1,
-        metalness: 1,
-      });
-    }
-  });
-
   const [yRotation, setYRotation] = useState(0);
 
   useEffect(() => {
@@ -31,18 +19,20 @@ export default function ThreeCanvas() {
   }, [yRotation, setYRotation]);
 
   return (
-    <Canvas
-      camera={{
-        fov: 60,
-        position: [0, 0, 150],
-        up: [0, 1, 0],
-      }}
-    >
-      <pointLight position={[0, 50, -50]} intensity={10} decay={0.5} />
-      <pointLight position={[0, 50, 150]} intensity={10} decay={0.5} />
-      <mesh rotation={[0.5, yRotation, 0]}>
-        <primitive object={model} />
-      </mesh>
-    </Canvas>
+    <div className="h-55">
+        <Canvas
+          camera={{
+            fov: 45,
+            position: [0, -45, 185],
+            up: [0, 1, 0],
+          }}
+        >
+          <pointLight position={[0, 50, -50]} intensity={10} decay={0.5} />
+          <pointLight position={[0, 50, 150]} intensity={10} decay={0.5} />
+          <mesh rotation={[0.5, yRotation, 0]}>
+            <primitive object={model} />
+          </mesh>
+        </Canvas>
+    </div>
   );
 }
